13f6fb92c9927a6bc15b46e71e38b26e340418c4
Derek Chafin <infomaniac50@gmail.com>
2018-04-05 01:10:03 -0500

    Append 64 to ubuntu vagrant folders.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --tag icu-test:trusty --file ubuntu-trusty/Dockerfile ./ubuntu-trusty/
-docker build --tag icu-test:xenial --file ubuntu-xenial/Dockerfile ./ubuntu-xenial/
+docker build --tag icu-test:trusty --file ubuntu-trusty64/Dockerfile ./ubuntu-trusty64/
+docker build --tag icu-test:xenial --file ubuntu-xenial64/Dockerfile ./ubuntu-xenial64/
 docker build --tag icu-test:artful --file ubuntu-artful/Dockerfile ./ubuntu-artful/
 docker build --tag icu-test:centos-7 --file centos-7/Dockerfile ./centos-7/
