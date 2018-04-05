63fce8636d2c3708db13385e0ec32c00621088f6
Derek Chafin <infomaniac50@gmail.com>
2018-04-05 02:50:41 -0500

    Move phpbrew to user bin folder and init phpbrew.

--- common/phpbrew.sh
+++ common/phpbrew.sh
@@ -2,3 +2,8 @@
 
 curl -L -O https://github.com/phpbrew/phpbrew/raw/master/phpbrew
 chmod u+x phpbrew
+mkdir -p ~/bin
+mv phpbrew ~/bin
+~/bin/phpbrew init
+echo 'export PATH=$PATH:~/bin' >> ~/.bashrc
+echo 'source ~/.phpbrew/bashrc' >> ~/.bashrc
