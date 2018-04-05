2e7656ab570d699f97a885ee6c457a4cadcdf3ea
Derek Chafin <infomaniac50@gmail.com>
2018-04-05 01:58:25 -0500

    Let editorconfig update our files.

--- bin/get-boxes.sh
+++ bin/get-boxes.sh
@@ -2,6 +2,6 @@
 
 vagrant box add --provider virtualbox archlinux/archlinux
 vagrant box add --provider virtualbox ubuntu/trusty64
-# vagrant box add --provider virtualbox ubuntu/xenial64
+vagrant box add --provider virtualbox ubuntu/xenial64
 vagrant box add --provider virtualbox centos/6
 vagrant box add --provider virtualbox centos/7
--- centos-7/phpbrew.sh
+++ centos-7/phpbrew.sh
@@ -2,4 +2,3 @@
 
 curl -L -O https://github.com/phpbrew/phpbrew/raw/master/phpbrew
 chmod u+x phpbrew
-
